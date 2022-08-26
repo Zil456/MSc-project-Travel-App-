7f74a4e06f9a3ea1f1874a9e8c0ac42f3489eb4f
Zil456 <Zilvinas_baltinas@yahoo.com>
2022-08-26 02:53:38 +0100

    code cleaned up

--- app/app.js
+++ app/app.js
@@ -145,7 +145,7 @@ app.get("/database_test", function (req, res) {
   });
 });
 
-// Start server on port 3000
+// Start listening to server on port 3000
 app.listen(3000, function () {
   console.log(`Server running at http://127.0.0.1:3000/`);
 });
